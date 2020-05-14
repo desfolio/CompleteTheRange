c245c45da1a2dcacc8461c9038f68e6ced78c5ee
Desmond Williams <desmond@desfolio.com>
2020-05-14 14:35:31 -0400

    added working code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CompleteTheRange
 {
@@ -6,7 +7,26 @@ namespace CompleteTheRange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var thisArray = new int[] { 1, 3, 4, 7, 9 };
+            var min = 0;
+            var max = 0;
+            var missCount = 0;
+
+            foreach (var num in thisArray)
+            {
+                min = Math.Min(thisArray[0], num);
+                max = Math.Max(thisArray[0], num);
+            }
+
+            for (int i = min; i < max; i++)
+            {
+                if (!thisArray.Contains(i))
+                {
+                    missCount++;
+                }
+            }
+
+            Console.WriteLine(missCount);
         }
     }
 }
